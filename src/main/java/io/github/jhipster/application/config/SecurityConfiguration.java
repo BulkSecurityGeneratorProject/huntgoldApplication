2577cc8e7c349a18f6a0a436018ca6702839dee6
Lujango <15583581921@163.com>
2018-07-26 23:36:56 +0800

    init

--- src/main/java/io/github/jhipster/application/config/SecurityConfiguration.java
+++ src/main/java/io/github/jhipster/application/config/SecurityConfiguration.java
@@ -150,7 +150,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .antMatchers("/management/**").hasAuthority(AuthoritiesConstants.ADMIN)
             .antMatchers("/v2/api-docs/**").permitAll()
             .antMatchers("/swagger-resources/configuration/ui").permitAll()
-            .antMatchers("/swagger-ui/index.html").hasAuthority(AuthoritiesConstants.ADMIN);
+            .antMatchers("/swagger-ui/index.html").hasAuthority(AuthoritiesConstants.ADMIN)
+        .and().authorizeRequests().antMatchers("/api").permitAll();
+
 
     }
 }
